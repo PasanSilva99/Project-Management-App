8bf358ab546f5574f001e4b606e4c97f4d5e2d60
Pasan Anuththara <pasananuththara19@gmail.com>
2022-03-24 14:17:25 +0530

    pmserver2

---
+++ PMService2/PMServer2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMServer2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(PMService2.ProjectService)))
+            {
+                host.Open();
+                Console.WriteLine("Project Server Started");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}

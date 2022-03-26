0c1ff3248961f0ec31db6591c12e0883b3b7225d
Pasan Anuththara <pasananuththara19@gmail.com>
2022-03-26 15:54:04 +0530

    login base

--- PCClient/PCClient/Model/DataStore.cs
+++ PCClient/PCClient/Model/DataStore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,39 @@ namespace PCClient.Model
 {
     public class DataStore
     {
-        public static String ApplicationName_Acronym { get; set; } = "CPMP";
+        public static String ApplicationName_Acronym { get; set; } = "Projent";
         public static String ApplicationName_full { get; set; } = "Collaborative Project Mnagement Platform";
+
+        public static bool CheckConnectivity()
+        {
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async static void IntializeLocalDatabase()
+        {
+
+        }
+
+        public async static void SyncServerDatabase()
+        {
+
+        }
+
+        public static bool ValidateUser(string email, string password)
+        {
+            return false;
+        }
+
+        public static User FindUser(string email, string password)
+        {
+            return null;
+        }
     }
 }
---
+++ PCClient/PCClient/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCClient.Model
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}

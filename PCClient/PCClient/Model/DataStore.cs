be204f42c7c7d821c9673f09f01c13ed49fbb1d5
Pasan Anuththara <pasananuththara19@gmail.com>
2022-03-26 13:55:25 +0530

    Login validation

---
+++ PCClient/PCClient/Model/DataStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCClient.Model
+{
+    public class DataStore
+    {
+        public static String ApplicationName_Acronym { get; set; } = "CPMP";
+        public static String ApplicationName_full { get; set; } = "Collaborative Project Mnagement Platform";
+    }
+}

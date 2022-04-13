792cc31d74fc23f7b09f85523e56d6b6d40ee34f
Pasan Anuththara <pasananuththara19@gmail.com>
2022-04-13 23:51:15 +0530

    Client IMp

--- Projent/Projent/Server/MainServer.cs
+++ Projent/Projent/Server/MainServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,11 @@ namespace Projent.Server
 {
     public class MainServer
     {
-        public static PMServer1.MainServiceClient mainServiceClient;
+        static PMServer1.MainServiceClient mainServiceClient;
         public static void InitializeServer()
         {
             mainServiceClient = new PMServer1.MainServiceClient(PMServer1.MainServiceClient.EndpointConfiguration.BasicHttpBinding_IMainService);
+            Debug.WriteLine("PMServer1 Client Started");
         }
     }
 }
--- Projent/Projent/Server/ProjectServer.cs
+++ Projent/Projent/Server/ProjectServer.cs
@@ -8,10 +8,10 @@ namespace Projent.Server
 {
     public class ProjectServer
     { 
-        public static PMServer2.ProjectServiceClient projectServiceClient;
+       // public static PMServer2.ProjectServiceClient projectServiceClient;
         public static void InitializeServer()
         {
-            projectServiceClient = new PMServer2.ProjectServiceClient(PMServer2.ProjectServiceClient.EndpointConfiguration.BasicHttpBinding_IProjectService);
+            //projectServiceClient = new PMServer2.ProjectServiceClient(PMServer2.ProjectServiceClient.EndpointConfiguration.BasicHttpBinding_IProjectService);
         }
     }
 }

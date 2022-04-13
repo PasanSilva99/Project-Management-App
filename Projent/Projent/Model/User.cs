fc9faddcb5e410aad7c7951babf9ecc1a289eed5
Pasan Anuththara <pasananuththara19@gmail.com>
2022-04-13 13:54:34 +0530

    Page Init

---
+++ Projent/Projent/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Projent.Model
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Password { get; set; }
+    }
+}

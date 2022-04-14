7ff3262fd21af7e0cf183b1a3bbb71e939f4e4c6
Pasan Anuththara <pasananuththara19@gmail.com>
2022-04-14 23:07:32 +0530

    Chat Client

--- Projent/Projent/Server/ProjectServer.cs
+++ Projent/Projent/Server/ProjectServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,11 @@ namespace Projent.Server
 {
     public class ProjectServer
     { 
-       // public static PMServer2.ProjectServiceClient projectServiceClient;
+        public static PMServer2.ProjectServiceClient projectServiceClient;
         public static void InitializeServer()
         {
-            //projectServiceClient = new PMServer2.ProjectServiceClient(PMServer2.ProjectServiceClient.EndpointConfiguration.BasicHttpBinding_IProjectService);
+            projectServiceClient = new PMServer2.ProjectServiceClient(PMServer2.ProjectServiceClient.EndpointConfiguration.BasicHttpBinding_IProjectService);
+            Debug.WriteLine("Project Client Initalized");
         }
     }
 }

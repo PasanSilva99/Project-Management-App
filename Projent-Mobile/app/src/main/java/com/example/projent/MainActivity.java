9c4ad889c36dc738034c0fe919a52a7fc5cf1478
Sandaru D Silva <sandarudsilva.dev@gmail.com>
2022-05-09 08:58:40 +0530

    projent mobile init

---
+++ Projent-Mobile/app/src/main/java/com/example/projent/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.projent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigator);
+        bottomNavigationView.setSelectedItemId(R.id.dashboard);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.dashboard:
+                        return true;
+
+                    case R.id.projects:
+                        startActivity(new Intent(getApplicationContext(),Projects.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(),Account.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}

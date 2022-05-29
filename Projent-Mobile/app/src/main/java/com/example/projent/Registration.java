96538732f53552d1258e71fa5387373f4c2002f0
Sandaru D Silva <sandarudsilva.dev@gmail.com>
2022-05-29 10:21:31 +0530

    Activity Navigation Added

--- Projent-Mobile/app/src/main/java/com/example/projent/Registration.java
+++ Projent-Mobile/app/src/main/java/com/example/projent/Registration.java
@@ -2,13 +2,40 @@ package com.example.projent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Registration extends AppCompatActivity {
+
+    Button btnBackR;
+    Button btnAddPhotoR;
+    Button btnRegisterR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        btnBackR = findViewById(R.id.btnBackR);
+        btnAddPhotoR = findViewById(R.id.btnAddPhotoR);
+        btnRegisterR = findViewById(R.id.btnRegisterR);
+
+        btnBackR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LogIn.class));
+            }
+        });
+
+        btnRegisterR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LogIn.class));
+            }
+        });
+
     }
 }
